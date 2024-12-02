633197acda16cb91ec2c5a355784801c2903bebf
palelyloitering <60718062+palelyloitering@users.noreply.github.com>
2024-12-02 14:38:07 +0000

    Tidying up menu

---
+++ assets/use-timeout.24756b40.js
@@ -0,0 +1,1 @@
+import{v as u}from"./Ripple.bbef8a44.js";import{q as i,o as m,g as s}from"./index.6aa36f1f.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
