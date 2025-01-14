9175c9f6c2684ffc9b9c607eefc74a30690f0553
palelyloitering <60718062+palelyloitering@users.noreply.github.com>
2025-01-14 17:42:28 +0000

    UI tidy up
    Renamed monthly to continuous
    Put fullscreen on menu bar
    Changed default month page size to bigger number

---
+++ assets/use-dark.f76caa75.js
@@ -0,0 +1,1 @@
+import{a as e}from"./index.94f5a4bb.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{o as a,u};
---
+++ assets/use-quasar.a0e51eb6.js
@@ -0,0 +1,1 @@
+import{l as a,q as r}from"./index.94f5a4bb.js";function u(){return a(r)}export{u};
